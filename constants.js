8b40b019493f1432c52cb10ca3b54eb2ea57f6e5
Shiv Bhuvanapalli <bikebaba@yahoo.com>
2017-03-06 15:45:22 -0500

    Test

--- constants.js
+++ constants.js
@@ -1,8 +1,4 @@
-var uri = "https://ecf-test.cm3b.aocms.uscourts.gov/n/beam/servlet/TransportRoom?";
-
-exports.serverConfigs = {
-  uri : "https://ecf-test.cm3b.aocms.uscourts.gov/n/beam/servlet/TransportRoom?"
-};
+var serverUri = "https://ecf-test.cm3b.aocms.uscourts.gov/n/beam/servlet/TransportRoom?";
 
 exports.adalConfiguration = {
   authority: 'https://login.microsoftonline.com/common',
@@ -14,7 +10,7 @@ exports.adalConfiguration = {
 
 exports.subscriptionConfiguration = {
   changeType: 'Created',
-  //notificationUrl: 'https://8b6b1d79.ngrok.io/listen',
+  //notificationUrl: 'https://c7a758f1.ngrok.io/listen',
   notificationUrl: uri +  'servlet=listen',
   //resource: 'me/events',
   resource: 'me/calendars/AQMkAGIzYTViZjgzLWYzN2QtNDUwMi1hMTM2LTNhNjk0MzJlNGQ3MwBGAAADolT90U35WEqIgZEtrX96eQcAbDi5PtVI3UuU5SGDPugV1QAAAgEGAAAAbDi5PtVI3UuU5SGDPugV1QAAAaJmFgAAAA==/events',
