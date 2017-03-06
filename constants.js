541f6deb802e8b5eae0021ecc0dcdb6dd56c4929
Shiv Bhuvanapalli <bikebaba@yahoo.com>
2017-03-06 11:57:01 -0500

    Changed listen url in constants.js

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
+var uri = "https://ecf-test.cm3b.aocms.uscourts.gov/n/beam/servlet/TransportRoom?";
+
 exports.serverConfigs = {
   uri : "https://ecf-test.cm3b.aocms.uscourts.gov/n/beam/servlet/TransportRoom?"
 };
@@ -5,7 +7,7 @@ exports.serverConfigs = {
 exports.adalConfiguration = {
   authority: 'https://login.microsoftonline.com/common',
   //redirectUri: 'http://localhost:12000/callback',
-  redirectUri: serverConfigs.uri +  'servlet=callback',
+  redirectUri: uri +  'servlet=callback',
   clientID: 'f19d3c30-0660-4f7f-96df-6dc78a686633',
   clientSecret: '2xizpbwJKnXEv4HGzaZFF9r2hT/lz/P/dbqBF3nhPSA='
 };
@@ -13,7 +15,7 @@ exports.adalConfiguration = {
 exports.subscriptionConfiguration = {
   changeType: 'Created',
   //notificationUrl: 'https://8b6b1d79.ngrok.io/listen',
-  notificationUrl: serverConfigs.uri +  'servlet=listen',
+  notificationUrl: uri +  'servlet=listen',
   //resource: 'me/events',
   resource: 'me/calendars/AQMkAGIzYTViZjgzLWYzN2QtNDUwMi1hMTM2LTNhNjk0MzJlNGQ3MwBGAAADolT90U35WEqIgZEtrX96eQcAbDi5PtVI3UuU5SGDPugV1QAAAgEGAAAAbDi5PtVI3UuU5SGDPugV1QAAAaJmFgAAAA==/events',
   clientState: 'cLIENTsTATEfORvALIDATION'
